1918b9b4c7192e5f234a7919c8c32ef052a7115c
AlbertCenderawan <03082190015@student.uph.edu>
2021-03-23 10:30:16 +0700

    Line Chart using C# (Design Form)

---
+++ PERTEMUAN 06/Line Chart/Line Chart.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ComputerGraphics.Class_Meeting.PERTEMUAN_06
+{
+    partial class FormStatistics
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbStatistics = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatistics)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbStatistics
+            // 
+            this.pbStatistics.Location = new System.Drawing.Point(0, 0);
+            this.pbStatistics.Name = "pbStatistics";
+            this.pbStatistics.Size = new System.Drawing.Size(800, 600);
+            this.pbStatistics.TabIndex = 0;
+            this.pbStatistics.TabStop = false;
+            // 
+            // FormStatistics
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 603);
+            this.Controls.Add(this.pbStatistics);
+            this.Name = "FormStatistics";
+            this.Text = "Line Chart";
+            this.Load += new System.EventHandler(this.Statistics_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbStatistics)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbStatistics;
+    }
+}
